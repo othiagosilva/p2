d24fd020108c7c1a7029b7cc34ec57f35f0c0095
Thiago Silva <othiagosilva.dev@gmail.com>
2022-12-02 17:39:36 -0300

    apis

--- treinador/views.py
+++ treinador/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import HttpResponse, render
 from .models import *
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
 from rest_framework import status
 from rest_framework.response import  Response
 from treinador.serializers import RendaSerializer
@@ -10,7 +8,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from .models import Aluno, Renda
-import psycopg2 as pg
 
 def index(request):
     return render(request, 'index.html')
@@ -134,66 +131,15 @@ def cadastrarRenda(request):
             return Response(rendas_serializer.data, status=status.HTTP_201_CREATED) 
         return Response(rendas_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['DELETE'])
+def deletarRenda(request, pk):
+    renda = Renda.objects.get(pk=pk) 
+    if request.method == 'DELETE': 
+        renda.delete() 
+        return Response('Renda deletada.', status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
 def consultarRenda(request):
     rendas = Renda.objects.all()
 
     return render(request, "consultar_renda.html", {"rendas":rendas})
-
-@api_view(['GET', 'POST', 'DELETE'])
-def renda_list(request):
-    # Retorna, cadastra e deleta as rendas
-    if request.method == 'GET':
-        rendas = Renda.objects.all()
-            
-        title = request.GET.get('title', None)
-        if title is not None:
-            rendas = rendas.filter(title__icontains=title)
-        
-        rendas_serializer = RendaSerializer(rendas, many=True)
-        return JsonResponse(rendas_serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        renda_data = JSONParser().parse(request)
-        rendas_serializer = RendaSerializer(data=renda_data)
-        if rendas_serializer.is_valid():
-            rendas_serializer.save()
-            return JsonResponse(rendas_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(rendas_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        count = Renda.objects.all().delete()
-        return JsonResponse({'message': '{} rendas deletadas.'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
- 
- 
-@api_view(['GET', 'PUT', 'DELETE'])
-def renda_detail(request, pk):
-    # Retorna a reda pelo código informado
-    try: 
-        renda = Renda.objects.get(pk=pk) 
-
-        if request.method == 'GET': 
-            rendas_serializer = RendaSerializer(renda) 
-            return JsonResponse(rendas_serializer.data)  
-
-        elif request.method == 'PUT': 
-            renda_data = JSONParser().parse(request) 
-            rendas_serializer = RendaSerializer(renda, data=renda_data) 
-            if rendas_serializer.is_valid(): 
-                rendas_serializer.save() 
-                return JsonResponse(rendas_serializer.data) 
-            return JsonResponse(rendas_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        elif request.method == 'DELETE': 
-            renda.delete() 
-            return JsonResponse({'message': 'Renda deletada.'}, status=status.HTTP_204_NO_CONTENT)
-    except Renda.DoesNotExist: 
-        return JsonResponse({'message': 'Ops! A renda não existe'}, status=status.HTTP_404_NOT_FOUND)
- 
-@api_view(['GET'])
-def tutorial_list_published(request):
-    # Retorna todas as rendas cadastradas
-    rendas = Renda.objects.filter(published=True)
-        
-    if request.method == 'GET': 
-        rendas_serializer = RendaSerializer(rendas, many=True)
-        return JsonResponse(rendas_serializer.data, safe=False)  
